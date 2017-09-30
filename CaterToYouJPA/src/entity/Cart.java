1ef26da39ca3fdc51d9ea2cf2f47d6be76bdc342
Samuel Ryzek <sryzek@gmail.com>
2017-09-30 13:27:57 -0600

    Sam add 8

--- CaterToYouJPA/src/entity/Cart.java
+++ CaterToYouJPA/src/entity/Cart.java
@@ -4,11 +4,17 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
 
 @Entity
-public class cart {
+public class Cart {
 	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@OneToOne
+	@JoinColumn(name="cart_id")
+	@OneToMany(mappedBy="cart")
 	private int id;
 }
