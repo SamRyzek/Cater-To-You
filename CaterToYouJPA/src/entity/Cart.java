b25b310969c7ff46a58c87c9982f4575538c6662
mustafa alici <mustafaalici@mustafas-MacBook-Pro.local>
2017-09-30 13:12:29 -0600

    wrote some test and made changes to database

--- CaterToYouJPA/src/entity/Cart.java
+++ CaterToYouJPA/src/entity/Cart.java
@@ -6,9 +6,19 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
-public class cart {
+public class Cart {
 	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int id;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	
 }
