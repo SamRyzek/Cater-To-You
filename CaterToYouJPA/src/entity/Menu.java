4c3dc3cf736b7b71c3dae4af428b072ece855740
Samuel Ryzek <sryzek@gmail.com>
2017-09-30 14:04:26 -0600

    Sam Update 11

--- CaterToYouJPA/src/entity/Menu.java
+++ CaterToYouJPA/src/entity/Menu.java
@@ -4,7 +4,9 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
 import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
 
 @Entity
 public class Menu {
@@ -13,6 +15,8 @@ public class Menu {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@OneToMany(mappedBy="Menu")
+	@OneToOne
+	@JoinColumn(name="menu_id")
 	private int id;
 
 	//gets and sets
