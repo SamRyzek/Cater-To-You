82ec44485d5d442dced7d8a2b0fbd771ad2a1566
C Bianchi <chrishbianchi@gmail.com>
2017-09-30 13:59:24 -0600

    chris

--- CaterToYouJPA/src/entity/Category.java
+++ CaterToYouJPA/src/entity/Category.java
@@ -12,5 +12,13 @@ public class Category {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int id;
 	
+	
 	private String category;
+	
+	@ManyToMany
+	@JoinTable("item_has_category",
+			joinColumns=@JoinColumn(name="category_id"),
+			inverseJoinColumns=@JoinColumn(name="item_id")
+			)
+	List<Item> itemList;
 }
