799c4a690cf7d777e47e7087a3dcb5b7d05618d1
mustafa alici <mustafaalici@mustafas-MacBook-Pro.local>
2017-10-01 16:36:43 -0600

    made a change to database there was no connection between customere and order

---
+++ CaterToYouJPA/test/entity/CustomerTest.java
@@ -0,0 +1,64 @@
+package entity;
+
+import static org.junit.Assert.assertEquals;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class CustomerTest {
+	
+	EntityManagerFactory emf = null;
+	EntityManager em = null;
+
+	@Before
+	public void setUp() {
+		emf = Persistence.createEntityManagerFactory("CaterToYou");
+		em = emf.createEntityManager();
+	}
+
+	@After
+	public void tearDown() {
+		em.close();
+		emf.close();
+	}
+
+	@Test
+	public void smokeTest() {
+		assertEquals(true, true);
+	}
+	
+	@Test
+	public void test_Customer_user_mapped() {
+		Customer c = em.find(Customer.class, 1);
+		assertEquals(13, c.getUser().getId());
+	}
+	
+	@Test
+	public void test_Customer_cart_mapped() {
+		Customer c = em.find(Customer.class, 1);
+		assertEquals(1, c.getCart().getId());
+	}
+	
+	@Test
+	public void test_Customer_image_mapped() {
+		Customer c = em.find(Customer.class, 1);
+		assertEquals(6, c.getImage().getId());
+	}
+	
+	@Test
+	public void test_Customer_address_mapped() {
+		Customer c = em.find(Customer.class, 1);
+		assertEquals(6, c.getAddress().getId());
+	}
+	
+	@Test
+	public void test_Customer_orderList_mapped() {
+		Customer c = em.find(Customer.class, 1);
+		assertEquals();
+	}
+}
