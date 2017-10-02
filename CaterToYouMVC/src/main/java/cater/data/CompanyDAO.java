de9d7a7a573283b286be319d188cd95727409074
Samuel Ryzek <sryzek@gmail.com>
2017-10-02 10:39:17 -0600

    DAO IMPL Update

--- CaterToYouMVC/src/main/java/cater/data/CompanyDAO.java
+++ CaterToYouMVC/src/main/java/cater/data/CompanyDAO.java
@@ -1,5 +1,41 @@
 package cater.data;
 
+import javax.servlet.http.HttpSession;
+
+import entity.Company;
+import entity.Employee;
+import entity.Image;
+import entity.Item;
+import entity.Menu;
+
 public interface CompanyDAO {
+	
+	
+	
+	public Company updateCompanyInfo(Company c, int id);
+	
+	public Menu updateMenuItem(Item i); 
+	public Menu addMenuItem(Item i); 
+	public Menu makeMenuItemInactive(Item i); 
+	
+	public Employee updateEmployee(Employee e);
+	public Employee addEmployee(Employee e);
+	public Employee makeEmployeeInactive(Employee e);
+	
+	public void updateImage(Image i); //could be a url for String even
+	public void addImage(Image i); //could be a url for String even
+	
+	
+
+	
+	
+//	we arent going to be adding or deleting menus, only affecting items within
+	
+	
+	
+//	need shit that takes in this shit
+//	company object
+//	menu object
+//	employee object
 
 }
